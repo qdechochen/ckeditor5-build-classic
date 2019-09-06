5c3505405ecf5dbecf95af220cdcbe6ba696bea0
Echo Chen <echo.chen@umoregroup.com>
2019-09-06 21:11:09 +0800

    all - zh-cn

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -11,6 +11,11 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -19,6 +24,7 @@ import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
@@ -27,6 +33,18 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Font from '@ckeditor/ckeditor5-font/src/font';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
+import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
+
+import MathType from '@wiris/mathtype-ckeditor5';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -37,6 +55,11 @@ ClassicEditor.builtinPlugins = [
 	Autoformat,
 	Bold,
 	Italic,
+	Underline,
+	Strikethrough,
+	Code,
+	Subscript,
+	Superscript,
 	BlockQuote,
 	CKFinder,
 	EasyImage,
@@ -44,6 +67,7 @@ ClassicEditor.builtinPlugins = [
 	Image,
 	ImageCaption,
 	ImageStyle,
+	ImageResize,
 	ImageToolbar,
 	ImageUpload,
 	Link,
@@ -52,43 +76,192 @@ ClassicEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	Alignment,
+	Font,
+	Mention,
+	RemoveFormat,
+	Highlight,
+	Autosave,
+	TextTransformation,
+	Indent,
+	IndentBlock,
+	TodoList,
+
+	MathType,
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
+			'undo',
+			'redo',
+			'removeFormat',
+			'|',
 			'heading',
+			'fontFamily',
+			'fontSize',
+			'fontColor',
+			'fontBackgroundColor',
 			'|',
 			'bold',
 			'italic',
+			'underline',
+			'strikethrough',
+			'code',
+			'subscript',
+			'superscript',
+			'alignment',
 			'link',
+			'|',
+			'outdent',
+			'indent',
+			'|',
 			'bulletedList',
 			'numberedList',
+			'todoList',
+			'|',
 			'imageUpload',
+			'mediaEmbed',
 			'blockQuote',
 			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
-		]
+			'|',
+			'MathType',
+			'ChemType',
+		],
 	},
 	image: {
 		toolbar: [
 			'imageStyle:full',
-			'imageStyle:side',
+			'imageStyle:alignLeft',
+			'imageStyle:alignCenter',
+			'imageStyle:alignRight',
 			'|',
-			'imageTextAlternative'
-		]
+			'imageTextAlternative',
+		],
+		styles: ['full', 'alignLeft', 'alignRight', 'alignCenter'],
+		resizeUnit: 'px',
 	},
 	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
+		contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	language: 'zh-cn',
+	fontFamily: {
+		options: [
+			'default',
+			'Arial, sans-serif',
+			'Courier New, Courier, monospace',
+		],
+	},
+	fontSize: {
+		options: [12, 14, 'default', 20, 24, 28, 40, 78],
+	},
+	fontColor: {
+		colors: [
+			{
+				color: 'hsl(0, 0%, 0%)',
+				label: 'Black',
+			},
+			{
+				color: 'hsl(0, 0%, 30%)',
+				label: 'Dim grey',
+			},
+			{
+				color: 'hsl(0, 0%, 60%)',
+				label: 'Grey',
+			},
+			{
+				color: 'hsl(0, 0%, 90%)',
+				label: 'Light grey',
+			},
+			{
+				color: 'hsl(0, 0%, 100%)',
+				label: 'White',
+				hasBorder: true,
+			},
+			{
+				color: 'hsl(0, 75%, 60%)',
+				label: 'Red',
+			},
+			{
+				color: 'hsl(30, 75%, 60%)',
+				label: 'Orange',
+			},
+			{
+				color: 'hsl(60, 75%, 60%)',
+				label: 'Yellow',
+			},
+			{
+				color: 'hsl(90, 75%, 60%)',
+				label: 'Light green',
+			},
+			{
+				color: 'hsl(120, 75%, 60%)',
+				label: 'Green',
+			},
+		],
+	},
+	fontBackgroundColor: {
+		colors: [
+			{
+				color: 'hsl(0, 75%, 60%)',
+				label: 'Red',
+			},
+			{
+				color: 'hsl(30, 75%, 60%)',
+				label: 'Orange',
+			},
+			{
+				color: 'hsl(60, 75%, 60%)',
+				label: 'Yellow',
+			},
+			{
+				color: 'hsl(90, 75%, 60%)',
+				label: 'Light green',
+			},
+			{
+				color: 'hsl(120, 75%, 60%)',
+				label: 'Green',
+			},
+		],
+	},
+	mediaEmbed: {},
+	alignment: {
+		options: ['left', 'right', 'center', 'justify'],
+	},
+	typing: {
+		transformations: {
+			extra: [
+				// Add some custom transformations – e.g. for emojis.
+				{ from: ':)', to: '🙂' },
+				{ from: ':+1:', to: '👍' },
+				{ from: ':tada:', to: '🎉' },
+			],
+		},
+	},
+	indentBlock: {
+		offset: 1,
+		unit: 'em',
+	},
+	link: {
+		decorators: {
+			addTargetToLinks: {
+				mode: 'manual',
+				label: 'Open in a new tab',
+				attributes: {
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+			},
+			toggleDownloadable: {
+				mode: 'manual',
+				label: 'Downloadable',
+				attributes: {
+					download: 'file',
+				},
+			},
+		},
+	},
 };
--- webpack.config.js
+++ webpack.config.js
@@ -7,60 +7,60 @@
 
 /* eslint-env node */
 
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
-const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
-const UglifyJsWebpackPlugin = require( 'uglifyjs-webpack-plugin' );
+const path = require('path');
+const webpack = require('webpack');
+const { bundler, styles } = require('@ckeditor/ckeditor5-dev-utils');
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
 	devtool: 'source-map',
 	performance: { hints: false },
 
-	entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
+	entry: path.resolve(__dirname, 'src', 'ckeditor.js'),
 
 	output: {
 		// The name under which the editor will be exported.
 		library: 'ClassicEditor',
 
-		path: path.resolve( __dirname, 'build' ),
+		path: path.resolve(__dirname, 'build'),
 		filename: 'ckeditor.js',
 		libraryTarget: 'umd',
-		libraryExport: 'default'
+		libraryExport: 'default',
 	},
 
 	optimization: {
 		minimizer: [
-			new UglifyJsWebpackPlugin( {
+			new UglifyJsWebpackPlugin({
 				sourceMap: true,
 				uglifyOptions: {
 					output: {
 						// Preserve CKEditor 5 license comments.
-						comments: /^!/
-					}
-				}
-			} )
-		]
+						comments: /^!/,
+					},
+				},
+			}),
+		],
 	},
 
 	plugins: [
-		new CKEditorWebpackPlugin( {
+		new CKEditorWebpackPlugin({
 			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
 			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
-			language: 'en',
-			additionalLanguages: 'all'
-		} ),
-		new webpack.BannerPlugin( {
+			language: 'zh-cn',
+			additionalLanguages: 'all',
+		}),
+		new webpack.BannerPlugin({
 			banner: bundler.getLicenseBanner(),
-			raw: true
-		} )
+			raw: true,
+		}),
 	],
 
 	module: {
 		rules: [
 			{
 				test: /\.svg$/,
-				use: [ 'raw-loader' ]
+				use: ['raw-loader'],
 			},
 			{
 				test: /\.css$/,
@@ -68,20 +68,22 @@ module.exports = {
 					{
 						loader: 'style-loader',
 						options: {
-							injectType: 'singletonStyleTag'
-						}
+							injectType: 'singletonStyleTag',
+						},
 					},
 					{
 						loader: 'postcss-loader',
-						options: styles.getPostCssConfig( {
+						options: styles.getPostCssConfig({
 							themeImporter: {
-								themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
+								themePath: require.resolve(
+									'@ckeditor/ckeditor5-theme-lark'
+								),
 							},
-							minify: true
-						} )
+							minify: true,
+						}),
 					},
-				]
-			}
-		]
-	}
+				],
+			},
+		],
+	},
 };
